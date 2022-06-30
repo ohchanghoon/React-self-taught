4a1300ff0d26fc18dbb33ed2af5c9ece8f95bb86
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-06-30 23:34:21 +0900

    play basic JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import Hello from './Hello'
 import './App.css';
 
 function App() {
+  const name = '창훈';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fondSize: 24,  // 기본 단위 px
+    padding: 'rem'  // 다른 단위 사용 시 문자열로 설정
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* 주석은 중괄호로 감싸야 주석이된다 ~ */}
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+      {/* <div>{name}</div>
+      <div>안녕히계세요</div> */}
+    </>
+  )
 }
 
 export default App;
