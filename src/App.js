034cc18d3d4647ce9821f828a08c49fa897ff456
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-07-03 00:09:08 +0900

    add: props를 통해 컴포넌트에 값 전달

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import Hello from './Hello'
-import './App.css';
+import Hello from './Hello';
+import Wrapper from './wrapper'
 
 function App() {
-  const name = '창훈';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fondSize: 24,  // 기본 단위 px
-    padding: 'rem'  // 다른 단위 사용 시 문자열로 설정
-  }
   return (
-    <>
-    {/* 주석은 중괄호로 감싸야 주석이된다 ~ */}
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-      {/* <div>{name}</div>
-      <div>안녕히계세요</div> */}
-    </>
-  )
+    <Wrapper>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </Wrapper>
+  );
 }
 
 export default App;
